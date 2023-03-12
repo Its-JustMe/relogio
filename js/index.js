6e61def67e6a31307e75ac71dc2dbb4c5ae5e553
Arthur Santos <108244695+xDef4lt@users.noreply.github.com>
2023-03-12 18:58:19 -0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,60 @@
+window.onload = () => {
+    // Guardando a tag h1 na variável
+    const h1 = document.querySelector('#msg');
+    // Chamando o objeto Date
+    const horario = new Date();
+    
+    // Pegando a hora, minuto e segundo
+    let h = horario.getHours();
+    let min = horario.getMinutes();
+    let seg = horario.getSeconds();
+
+    // Pegando os elementos do relógio
+    const hora = document.querySelector('.hora');
+    const minuto = document.querySelector('.minuto');
+    const segundo = document.querySelector('.segundo');
+
+    hora.innerHTML = h;
+    minuto.innerHTML = min;
+    segundo.innerHTML = seg;
+
+    // Função para atualizar o contador
+    setInterval(() => {
+        seg++;
+        seg = seg < 10 ? '0' + seg : seg;
+
+        if (seg > 59) {
+            seg = '00';
+
+            min = min < 10 ? '0' + min : min;
+            min++;
+
+            if (min > 59) {
+                min = '00';
+
+                h = h < 10 ? '0' + h: h;
+                h++;
+
+                if (h > 23) {
+                    h = '00';
+                }
+                hora.innerHTML = h;
+            }
+
+            minuto.innerHTML = min;
+        }
+        segundo.innerHTML = seg;
+
+        // Atualizando a mensagem
+        if (h >= 5 && h < 12) {
+            h1.innerHTML = 'Bom dia!';
+        } else if (h >= 12 && h < 18) {
+            h1.innerHTML = 'Boa tarde!';
+        } else if (h >= 18 < h < 23) {
+            h1.innerHTML = 'Boa noite!';
+        } else {
+            h1.innerHTML = 'Boa madrugada!';
+        }
+
+    }, 1000);
+}
